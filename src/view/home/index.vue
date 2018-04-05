67db0e30c3435ebec24fb8d23e79f530d2843bd6
shibeibei <shibeibei@chinamobo.com>
2018-04-05 15:14:06 +0800

    首页

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -7,189 +7,63 @@
             </router-link>
         </mt-header>
 
-        <div class=" b-c-b">
-            <div class="home">
-                <img  class="index-picture p-re" src="../../assets/index/shouye_dingbu_img@2x.png"/>
-                <div class="ub ub-pc words ">
-                    当日总收入（元）
+        <div class="">
+
+            <div class="home" @click="goBlack(5)">
+                <span class="black-title">黑名单</span>
+                <img class="p-re" src="../../assets/index/black_img.png"/>
+            </div>
+
+            <!--当日入库-->
+            <div class="ub today">
+                <div class="ub-f1 bd-r">
+                    <div class="number">480</div>
+                    <div class="words">当日入库</div>
                 </div>
-                <div class=" ub ub-pc totalNumber">
-                    +5758.4
+                <div class="ub-f1 bd-r">
+                    <div class="number">180</div>
+                    <div class="words">当日下单</div>
                 </div>
-                <div class="ub ub-pc total">
-                    本月收入（元）215000
-                </div>
-                <img @click="goMy(5)" src="../../assets/index/shouye_touxiang_img@2x.png" class="p-ab head-img ">
-                <div class="p-b-16">
-                    <div class="black" @click="goBlack(5)">
-                        <img src="../../assets/index/black_img.png"/>
-                        <span class="black-title">黑名单</span>
-                    </div>
+                <div class="ub-f1">
+                    <div class="number">80</div>
+                    <div class="words">暂存下单</div>
                 </div>
             </div>
 
-            <!--总体情况-->
-            <div class=" payments ">
-                <div class="total-name">当日收支</div>
-                <div>
-                    <div class="ub p-re bd-b ">
-                        <div class="ub ub-f1 bd-r" @click="goEarning">
-                            <div class=" ">
-                                <img src="../../assets/index/sale_icon.png"/></div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">当日销售额(元)</div>
+            <!--货品-->
+            <div class="goods">
+                <div class="ub">
+                    <div class="ub-f1">
+                        <div class="bd-r bd-b">
+                            <div>
+                                <img class="goods-image left-imge" src="../../assets/index/total_income_icon.png"/>
                             </div>
+                            <div class="name">货品入库</div>
                         </div>
-                        <div class="ub ub-f1" @click="goEarning">
-                            <div class=" ">
-                                <img src="../../assets/index/total_income_icon.png"/></div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">当日总收入(元)</div>
+                        <div class="bd-r">
+                            <div>
+                                <img class="goods-image left-imge" src="../../assets/index/total_income_icon.png"/>
                             </div>
+                            <div class="name">暂存订单</div>
                         </div>
                     </div>
-                    <div class="ub p-re p-b-10 ">
-                        <div class="ub ub-f1 bd-r" @click="goPaylist">
-                            <div class=" ">
-                                <img src="../../assets/index/total_pay_icon.png"/>
-                            </div>
+                    <div class="ub-f1">
+                        <div class="bd-b">
                             <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">当日总支出(元)</div>
+                                <img class="goods-image right-img" src="../../assets/index/total_income_icon.png"/>
                             </div>
+                            <div class="name">货品损坏</div>
                         </div>
-                        <div class="ub ub-f1" @click="creditCustomer">
-                            <div class=" ">
-                                <img src="../../assets/index/total_credit_icon.png"/>
+
+                        <div class=" bd-b">
+                            <div>
+                                <img class="goods-image right-img" src="../../assets/index/total_income_icon.png"/>
                             </div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money ">当日总赊账(元)</div>
-                            </div>
+                            <div class="name">车次计算</div>
                         </div>
                     </div>
                 </div>
 
-            </div>
-            <!--当日收支    -->
-            <div class="payments">
-                <div class="total-name">总体情况</div>
-                <div>
-                    <div class="ub p-re bd-b ">
-                        <div class="ub ub-f1  bd-r" @click="goEarning">
-                            <div class=" ">
-                                <img src="../../assets/index/sale_icon.png"/></div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">总销售额(元)</div>
-                            </div>
-                        </div>
-                        <div class="ub ub-f1" @click="goEarning">
-                            <div class=" ">
-                                <img src="../../assets/index/total_income_icon.png"/></div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">总收入(元)</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ub p-re p-b-10 ">
-                        <div class="ub ub-f1 bd-r" @click="goPaylist">
-                            <div class=" ">
-                                <img src="../../assets/index/total_pay_icon.png"/>
-                            </div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">支出总金额(元)</div>
-                            </div>
-                        </div>
-                        <div class="ub ub-f1" @click="creditCustomer">
-                            <div class=" ">
-                                <img src="../../assets/index/total_credit_icon.png"/>
-                            </div>
-                            <div class="">
-                                <div class="number">15678.0</div>
-                                <div class="money">赊账总金额(元)</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <!--坏账报警-->
-            <div class="debt m-b-10">
-                <div class="total-name">
-                    坏账报警
-                </div>
-                <div class="debt-content">
-
-                    <div class="ub  bd-b" @click="goOrder">
-                        <div class="ub-f1">
-                            <img class="debt-img" src="../../assets/index/bad_debt_red.png"/>
-                        </div>
-                        <div class="ub-f4  debt-common">
-                            <div class="f-s-30 c-3">强东哥哥</div>
-                            <div class="date">最长时间：7天</div>
-                        </div>
-
-                        <div class="ub-f1  debt-common">
-                            <div class="">
-                                <div class="f-s-30 c-red">156698</div>
-                                <div class="repay">待还款</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="ub  bd-b" @click="goOrder">
-                        <div class="ub-f1">
-                            <img class="debt-img" src="../../assets/index/bad_debt_yellow.png"/>
-                        </div>
-                        <div class="ub-f4  debt-common">
-                            <div class="f-s-30 c-3">强东妈妈</div>
-                            <div class="date">最长时间：7天</div>
-                        </div>
-                        <div class="ub-f1 ub ub-pe debt-common">
-                            <div class="">
-                                <div class="f-s-30 c-yellow">156698</div>
-                                <div class="repay">待还款</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="ub bd-b" @click="goOrder">
-                        <div class="ub-f1">
-                            <img class="debt-img" src="../../assets/index/bad_debt_orange.png"/>
-                        </div>
-                        <div class="ub-f4  debt-common">
-                            <div class="f-s-16 c-3">强东爸爸</div>
-                            <div class="date">最长时间：7天</div>
-                        </div>
-                        <div class="ub-f1 ub ub-pe  debt-common">
-                            <div class="">
-                                <div class="f-s-16 c-orange">156698</div>
-                                <div class="repay">待还款</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="ub" @click="goOrder">
-                        <div class="ub-f1">
-                            <img class="debt-img" src="../../assets/index/bad_debt_green.png"/>
-                        </div>
-                        <div class="ub-f4  debt-common">
-                            <div class="f-s-16 c-3">强东姐姐</div>
-                            <div class="date">最长时间：7天</div>
-                        </div>
-                        <div class="ub-f1 ub ub-pe  debt-common">
-                            <div class="">
-                                <div class="f-s-16 c-blue">156698</div>
-                                <div class="repay">待还款</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
@@ -220,7 +94,7 @@
                 this.$router.push({name: 'creditOrder'});
             },
             //跳转至支出列表
-			goPaylist(){
+            goPaylist(){
                 this.$router.push({name: 'pay'});
             },
             getList(){
@@ -239,91 +113,47 @@
     }
 
     .home {
+        padding: 0.42rem 0.3rem 0.32rem;
         background-color: #ffffff;
-        padding: 0 0.3rem;
-        margin-top: 0.2rem;
-        .index-picture {
-            width: 100%;
-        }
-        .words {
-            top: 0.53rem;
-            font-size: 0.26rem;
-            padding-left: 0.3rem;
+        .black-title {
+            top: 0.8rem;
+            left: 0.75rem;
+            color: #424248;
+            font-size: 0.3rem;
             position: absolute;
-            color: #ffffff;
-        }
-        .totalNumber {
-            top: 1.23rem;
-            color: #ffffff;
-            position: absolute;
-            font-size: 0.72rem;
-            padding-left: 0.3rem;
-        }
-        .total {
-            top: 2.65rem;
-            color: white;
-            font-size: 0.28rem;
-            padding-left: 0.3rem;
-            position: absolute;
-
-        }
-        .head-img {
-            top: 0.5rem;
-            right: 0.42rem;
+            z-index: 100;
         }
     }
-
-    .black-title {
-        top: 3.95rem;
-        left: 0.75rem;
-        color: #424248;
-        font-size: 0.3rem;
-        position: absolute;
-    }
-
-    .payments, .debt {
+    .today{
         background-color: white;
-        padding: 0.2rem;
-        margin-top: 0.22rem;
-        .total-name {
-            padding: 0.12rem 0 0 0.4rem;
-            color: #333;
-            font-size: 0.28rem;
+        margin: 0.22rem 0;
+        padding: 0.32rem 0;
+
+        .number{
+            font-size: 0.42rem;
+            color: #33d57c;
+            padding: 0 0.9rem;
         }
-        .number {
-            font-size: 0.36rem;
-            color: #333333;
-            padding: 0.42rem 0 0.14rem 0;
-        }
-        .money{
+        .words{
             font-size: 0.24rem;
-            color: #999999;
+            color: #666;
+            padding-left: 0.8rem;
+            padding-top: 0.24rem;
         }
-        img {
-            width: 0.8125rem;
-            padding: 0.42rem 0.28rem 0.32rem 0.3rem;
+    }
+    .goods{
+        background-color: white;
+        padding: 0 0.56rem;
+        .goods-image{
+            width: 1.72rem;
+            padding: 0.52rem 0.735rem 0 0.735rem;
         }
-        .debt-img {
-            width: 0.54rem;
-            height: 0.47rem;
-            padding: 0.63rem 0 0.46rem 0;
-        }
-        .debt-content {
-            padding: 0 0.4rem;
-        }
-        .debt-common {
-            padding-top: 0.4rem;
-            .repay{
-                text-align: right;
-                font-size: 0.22rem;
-                color: #808080;
-                padding-top: 0.24rem;
-            }
-            .date{
-                padding-top: 0.24rem;
-                font-size: 0.22rem;
-                color: #808080;
-            }
+
+        .name{
+            font-size: 0.3rem;
+            color: #333333;
+            text-align: center;
+            padding: 0.32rem  0 0.52rem;
         }
     }
 
