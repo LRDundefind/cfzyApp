0cf9b760931938282301f74ce1b05a72a5b93de8
shibeibei <shibeibei@chinamobo.com>
2018-04-19 20:25:44 +0800

    修改密码

--- src/view/my/alterPassword.vue
+++ src/view/my/alterPassword.vue
@@ -12,17 +12,17 @@
         <div class="login_cont p-t-40">
             <div class="bg1">
                 <span class="usericonBg"></span>
-                <input v-model="oldPassword" placeholder="旧密码" type="password" class="usericon ">
+                <input v-model="params.old_password" placeholder="旧密码" type="password" class="usericon ">
             </div>
 
             <div class="bg3">
                 <span class="usericonBg"></span>
-                <input v-model="newPassword" placeholder="新密码" type="password" class="usericonPhone">
+                <input v-model="params.password" placeholder="新密码" type="password" class="usericonPhone">
             </div>
 
             <div class="bg2">
                 <span class="passIconBg"></span>
-                <input v-model="confirmPassword" placeholder="确认密码" type="password" class="passIcon ">
+                <input v-model="params.confirmPassword" placeholder="确认密码" type="password" class="passIcon ">
             </div>
 
             <div @click="alterPass" class="findbtn">更改登录密码</div>
@@ -77,13 +77,18 @@
 </style>
 <script>
     import {Toast} from 'mint-ui';
+    import {my} from '@/services/apis/my'
+
     export default {
         name: 'login',
         data () {
             return {
-                oldPassword: '',
-                newPassword: '',
-                confirmPassword: ''
+                params: {
+                    old_password: '',
+                    password: '',
+                    confirmPassword: ''
+                }
+
             }
         },
         mounted () {
@@ -91,17 +96,49 @@
         },
         methods: {
             alterPass(){
-                if (this.oldPassword && this.newPassword && this.confirmPassword) {
-                    if (this.newPassword != this.confirmPassword) {
+                if (this.params.old_password && this.params.password && this.params.confirmPassword) {
+                    if (!(new RegExp(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/).test(this.params.old_password))) {
+                        Toast({
+                            message: '旧密码格式有误',
+                            position: 'middle',
+                            duration: 5000
+                        });
+                    } else if (!(new RegExp(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/).test(this.params.password))) {
+                        Toast({
+                            message: '新密码格式有误',
+                            position: 'middle',
+                            duration: 5000
+                        });
+                    } else if (this.params.password != this.params.confirmPassword) {
                         Toast({
                             message: '两次输入密码不一致',
                             position: 'middle',
                             duration: 5000
                         });
+                    } else {
+                        let data = this.params;
+                        delete data.confirmPassword;
+                        console.log(data);
+                        my.password(data).then(response => {
+                            if (response.data.status == 'Y') {
+                                Toast({
+                                    message: '密码修改成功',
+                                    position: 'middle',
+                                    duration: 5000
+                                });
+                                this.$router.push({name: 'login'});
+                            } else if (response.data.status == 'N') {
+                                Toast({
+                                    message: response.data.error_msg,
+                                    position: 'middle',
+                                    duration: 5000
+                                });
+                            }
+                        })
                     }
                 } else {
                     Toast({
-                        message: '密码不可为空',
+                        message: '请完善信息',
                         position: 'middle',
                         duration: 5000
                     });
