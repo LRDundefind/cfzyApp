f72943fe4dc7f341cf9938af2312ff41e73246bf
WXY <wangxinyu@chinamobo.com>
2018-05-02 09:20:30 +0800

    尝试添加百度地图

--- src/view/logistics/logistics.vue
+++ src/view/logistics/logistics.vue
@@ -4,9 +4,12 @@
 		</mt-header>
 		<!--物流-->
 		<div class="page-main logistics">
-			<div class="not-online">
+			<!-- <div class="not-online">
 				<img src="../../assets/wuliu.png">
 				<div>敬请期待...</div>
+			</div> -->
+			<div id="container">
+
 			</div>
 		</div>
 	</div>
@@ -21,7 +24,19 @@ export default {
         }
     },
     mounted () {
-
+		    // 百度地图API功能
+            // 创建Map实例
+            var map = new BMap.Map("XSDFXPage",{enableMapClick:true});
+            // 初始化地图,设置中心点坐标和地图级别
+            map.centerAndZoom(new BMap.Point(116.4035,39.915), 11);
+            // 添加地图类型控件
+            map.addControl(new BMap.MapTypeControl());  
+            // 设置地图显示的城市 此项是必须设置的
+            map.setCurrentCity("北京"); 
+			
+			setTimeout(function(){
+                map.panTo(new BMap.Point(113.262232,23.154345));
+            }, 2000);  
     },
     methods: {
             
@@ -29,7 +44,10 @@ export default {
 }
 </script>
 <style scoped rel="stylesheet/scss" lang="scss">
-
+#container{
+	width: 100%;
+	height: 100%;
+}
 .not-online{
 	position: fixed;
 	width: 100%;
