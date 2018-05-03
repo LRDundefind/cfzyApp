e83292e80a1a8f3e875cbdb59e4ea2c24547d173
shibeibei <shibeibei@chinamobo.com>
2018-05-03 17:23:58 +0800

    货品入库

--- src/view/goods/storage.vue
+++ src/view/goods/storage.vue
@@ -55,8 +55,8 @@
 
                     <div class="basic-list">
                         <p class="clearfix">产地<input type="text" v-model="stall.origin"></p>
-                        <p class="clearfix" @change="upload1" style="position: relative">产地证明
-                            <input type="file" id="upload" accept="image" @change="upload1" style="opacity: 0.4">
+                        <p class="clearfix" style="position: relative">产地证明
+                            <input type="file" id="upload" accept="image" @change="upload1($event,'111')" style="opacity: 0.4">
                             <span class="upload">点击上传<img class="right-icon"
                                                           src="../../assets/index/gray-right-icon.png"/></span>
                         </p>
@@ -308,7 +308,8 @@
                 }
             },
 
-            upload1 (e) {
+            upload1 (e,ty) {
+                console.log(ty);
                 let files = e.target.files || e.dataTransfer.files;
                 if (!files.length) return;
                 this.picValue = files[0];
@@ -353,6 +354,72 @@
             },
             postImg(){
                 console.log(this.headerImage)
+            },
+            compress(img, Orientation) {
+                let canvas = document.createElement("canvas");
+                let ctx = canvas.getContext('2d');
+                //瓦片canvas
+                let tCanvas = document.createElement("canvas");
+                let tctx = tCanvas.getContext("2d");
+                let initSize = img.src.length;
+                let width = img.width;
+                let height = img.height;
+                //如果图片大于四百万像素，计算压缩比并将大小压至400万以下
+                let ratio;
+                if ((ratio = width * height / 4000000) > 1) {
+                    console.log("大于400万像素")
+                    ratio = Math.sqrt(ratio);
+                    width /= ratio;
+                    height /= ratio;
+                } else {
+                    ratio = 1;
+                }
+                canvas.width = width;
+                canvas.height = height;
+                //        铺底色
+                ctx.fillStyle = "#fff";
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                //如果图片像素大于100万则使用瓦片绘制
+                let count;
+                if ((count = width * height / 1000000) > 1) {
+                    console.log("超过100W像素");
+                    count = ~~(Math.sqrt(count) + 1); //计算要分成多少块瓦片
+                    //            计算每块瓦片的宽和高
+                    let nw = ~~(width / count);
+                    let nh = ~~(height / count);
+                    tCanvas.width = nw;
+                    tCanvas.height = nh;
+                    for (let i = 0; i < count; i++) {
+                        for (let j = 0; j < count; j++) {
+                            tctx.drawImage(img, i * nw * ratio, j * nh * ratio, nw * ratio, nh * ratio, 0, 0, nw, nh);
+                            ctx.drawImage(tCanvas, i * nw, j * nh, nw, nh);
+                        }
+                    }
+                } else {
+                    ctx.drawImage(img, 0, 0, width, height);
+                }
+                //修复ios上传图片的时候 被旋转的问题
+                if (Orientation != "" && Orientation != 1) {
+                    switch (Orientation) {
+                        case 6://需要顺时针（向左）90度旋转
+                            this.rotateImg(img, 'left', canvas);
+                            break;
+                        case 8://需要逆时针（向右）90度旋转
+                            this.rotateImg(img, 'right', canvas);
+                            break;
+                        case 3://需要180度旋转
+                            this.rotateImg(img, 'right', canvas);//转两次
+                            this.rotateImg(img, 'right', canvas);
+                            break;
+                    }
+                }
+                //进行最小压缩
+                let ndata = canvas.toDataURL('image/jpeg', 0.1);
+                console.log('压缩前：' + initSize);
+                console.log('压缩后：' + ndata.length);
+                console.log('压缩率：' + ~~(100 * (initSize - ndata.length) / initSize) + "%");
+                tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
+                return ndata;
             },
 
 
