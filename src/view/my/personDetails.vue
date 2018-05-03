eb8f4449f98a92f32c1e695c5b1c1467aaabe4d6
shibeibei <shibeibei@chinamobo.com>
2018-05-03 14:16:18 +0800

    完善用户资料

--- src/view/my/personDetails.vue
+++ src/view/my/personDetails.vue
@@ -12,12 +12,12 @@
                         <div class="sc">
                             <div style="opacity: 0" class="picture"
                                  :style="'backgroundImage:url('+headerImage+')'"></div>
-                                <!-- <div>{{headerImage}}</div> -->
+                            <!-- <div>{{headerImage}}</div> -->
                             <input type="file" id="upload" accept="image" @change="upload1" style="opacity: 0">
                             <div>
-                                 <img  class="header-img" :src="personalData.headImg"/>
+                                <img class="header-img" :src="personalData.headImg"/>
                             </div>
-                           
+
                         </div>
                     </div>
                 </div>
@@ -49,9 +49,9 @@
                 phone: this.$route.params.phone,
                 yanNumber: this.$route.params.yanNumber,
                 headerImage: '',
-                picValue:'',
-                selName:this.$route.params.selName,
-                headImg:this.$route.params.headImg,
+                picValue: '',
+                selName: this.$route.params.selName,
+                headImg: this.$route.params.headImg,
             }
         },
         mounted () {
@@ -87,9 +87,15 @@
 //                        if(this.personalData.headImg){
 //                            this.personalData.headImg = imgpath+ this.personalData.headImg;
 //                        }
-                        if (this.phone) {this.personalData.phone = this.phone;}
-                        if (this.selName) {this.personalData.selName = this.selName;}
-                        if (this.headImg) {this.personalData.headImg = this.headImg;}
+                        if (this.phone) {
+                            this.personalData.phone = this.phone;
+                        }
+                        if (this.selName) {
+                            this.personalData.selName = this.selName;
+                        }
+                        if (this.headImg) {
+                            this.personalData.headImg = this.headImg;
+                        }
                     } else {
 
                     }
@@ -104,12 +110,12 @@
             imgPreview (file) {
                 var self = this;
                 let Orientation;
-                
+
                 //去获取拍照时的信息，解决拍出来的照片旋转问题  
                 Exif.getData(file, function () {
                     Orientation = Exif.getTag(this, 'Orientation');
                 });
-               
+
                 // 看支持不支持FileReader  
                 // if (!file || !window.FileReader) return;
 
@@ -121,7 +127,7 @@
                     // 读取成功后的回调  
                     reader.onloadend = function () {
                         let result = this.result;
-                        
+
                         let img = new Image();
                         img.src = result;
                         //判断图片是否大于100K,是就直接上传，反之压缩图片  
@@ -144,109 +150,109 @@
             postImg(){
                 // console.log(this.headerImage)
             },
-            compress(img,Orientation) {  
-                let canvas = document.createElement("canvas");  
-                let ctx = canvas.getContext('2d');  
-                    //瓦片canvas  
-                let tCanvas = document.createElement("canvas");  
-                let tctx = tCanvas.getContext("2d");  
-                let initSize = img.src.length;  
-                let width = img.width;  
-                let height = img.height;  
+            compress(img, Orientation) {
+                let canvas = document.createElement("canvas");
+                let ctx = canvas.getContext('2d');
+                //瓦片canvas
+                let tCanvas = document.createElement("canvas");
+                let tctx = tCanvas.getContext("2d");
+                let initSize = img.src.length;
+                let width = img.width;
+                let height = img.height;
                 //如果图片大于四百万像素，计算压缩比并将大小压至400万以下  
-                let ratio;  
-                if ((ratio = width * height / 4000000) > 1) {  
-                    console.log("大于400万像素")  
-                    ratio = Math.sqrt(ratio);  
-                    width /= ratio;  
-                    height /= ratio;  
-                } else {  
-                    ratio = 1;  
-                }  
-                canvas.width = width;  
-                canvas.height = height;  
-            //        铺底色  
-                ctx.fillStyle = "#fff";  
-                ctx.fillRect(0, 0, canvas.width, canvas.height);  
+                let ratio;
+                if ((ratio = width * height / 4000000) > 1) {
+                    console.log("大于400万像素")
+                    ratio = Math.sqrt(ratio);
+                    width /= ratio;
+                    height /= ratio;
+                } else {
+                    ratio = 1;
+                }
+                canvas.width = width;
+                canvas.height = height;
+                //        铺底色
+                ctx.fillStyle = "#fff";
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
                 //如果图片像素大于100万则使用瓦片绘制  
-                let count;  
-                if ((count = width * height / 1000000) > 1) {  
-                    console.log("超过100W像素");  
+                let count;
+                if ((count = width * height / 1000000) > 1) {
+                    console.log("超过100W像素");
                     count = ~~(Math.sqrt(count) + 1); //计算要分成多少块瓦片  
-            //            计算每块瓦片的宽和高  
-                    let nw = ~~(width / count);  
-                    let nh = ~~(height / count);  
-                    tCanvas.width = nw;  
-                    tCanvas.height = nh;  
-                    for (let i = 0; i < count; i++) {  
-                    for (let j = 0; j < count; j++) {  
-                        tctx.drawImage(img, i * nw * ratio, j * nh * ratio, nw * ratio, nh * ratio, 0, 0, nw, nh);  
-                        ctx.drawImage(tCanvas, i * nw, j * nh, nw, nh);  
-                    }  
-                    }  
-                } else {  
-                    ctx.drawImage(img, 0, 0, width, height);  
-                }  
+                    //            计算每块瓦片的宽和高
+                    let nw = ~~(width / count);
+                    let nh = ~~(height / count);
+                    tCanvas.width = nw;
+                    tCanvas.height = nh;
+                    for (let i = 0; i < count; i++) {
+                        for (let j = 0; j < count; j++) {
+                            tctx.drawImage(img, i * nw * ratio, j * nh * ratio, nw * ratio, nh * ratio, 0, 0, nw, nh);
+                            ctx.drawImage(tCanvas, i * nw, j * nh, nw, nh);
+                        }
+                    }
+                } else {
+                    ctx.drawImage(img, 0, 0, width, height);
+                }
                 //修复ios上传图片的时候 被旋转的问题  
-                if(Orientation != "" && Orientation != 1){  
-                    switch(Orientation){  
-                    case 6://需要顺时针（向左）90度旋转  
-                        this.rotateImg(img,'left',canvas);  
-                        break;  
-                    case 8://需要逆时针（向右）90度旋转  
-                        this.rotateImg(img,'right',canvas);  
-                        break;  
-                    case 3://需要180度旋转  
-                        this.rotateImg(img,'right',canvas);//转两次  
-                        this.rotateImg(img,'right',canvas);  
-                        break;  
-                    }  
-                }  
+                if (Orientation != "" && Orientation != 1) {
+                    switch (Orientation) {
+                        case 6://需要顺时针（向左）90度旋转
+                            this.rotateImg(img, 'left', canvas);
+                            break;
+                        case 8://需要逆时针（向右）90度旋转
+                            this.rotateImg(img, 'right', canvas);
+                            break;
+                        case 3://需要180度旋转
+                            this.rotateImg(img, 'right', canvas);//转两次
+                            this.rotateImg(img, 'right', canvas);
+                            break;
+                    }
+                }
                 //进行最小压缩  
-                let ndata = canvas.toDataURL('image/jpeg', 0.1);  
-                console.log('压缩前：' + initSize);  
-                console.log('压缩后：' + ndata.length);  
-                console.log('压缩率：' + ~~(100 * (initSize - ndata.length) / initSize) + "%");  
-                tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;  
-                return ndata;  
-                },  
-                        goPhone(){
-                            this.$router.push({
-                                name: 'replacePhone',
-                                params: {selName: this.personalData.selName, headImg: this.personalData.headImg}
-                            });
-                        },
-                        savePersonal(){
-                            let data = this.personalData;
-                            delete data.createTime;
-                            delete data.password;
-                            delete data.salt;
-                            delete data.sid;
-                            if(this.yanNumber){
-                                data.code = this.yanNumber;
-                            }
-                            //console.log(data);
-                            my.alterPersonal(data).then(response => {
-                                if (response.data.status == 'Y') {
-                                    Toast({
-                                        message: '保存成功',
-                                        position: 'middle',
-                                        duration: 1000
-                                    });
-                                    this.info();
-                                    setTimeout(()=>{
-                                        this.$router.push({ name: 'my'})
-                                    },2000)
-                                    
-                                } else {
-                                    Toast({
-                                        message: response.data.error_msg,
-                                        position: 'middle',
-                                        duration: 3000
-                                    });
-                                }
-                            })
-                        },
+                let ndata = canvas.toDataURL('image/jpeg', 0.1);
+                console.log('压缩前：' + initSize);
+                console.log('压缩后：' + ndata.length);
+                console.log('压缩率：' + ~~(100 * (initSize - ndata.length) / initSize) + "%");
+                tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
+                return ndata;
+            },
+            goPhone(){
+                this.$router.push({
+                    name: 'replacePhone',
+                    params: {selName: this.personalData.selName, headImg: this.personalData.headImg}
+                });
+            },
+            savePersonal(){
+                let data = this.personalData;
+                delete data.createTime;
+                delete data.password;
+                delete data.salt;
+                delete data.sid;
+                if (this.yanNumber) {
+                    data.code = this.yanNumber;
+                }
+                //console.log(data);
+                my.alterPersonal(data).then(response => {
+                    if (response.data.status == 'Y') {
+                        Toast({
+                            message: '保存成功',
+                            position: 'middle',
+                            duration: 1000
+                        });
+                        this.info();
+                        setTimeout(() => {
+                            this.$router.push({name: 'my'})
+                        }, 2000)
+
+                    } else {
+                        Toast({
+                            message: response.data.error_msg,
+                            position: 'middle',
+                            duration: 3000
+                        });
+                    }
+                })
+            },
         }
     }
 </script>
