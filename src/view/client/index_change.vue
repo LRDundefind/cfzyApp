de31af6196119d24e1a55fdaae5f9d1fe79bd3d5
WXY <wangxinyu@chinamobo.com>
2018-05-05 15:25:38 +0800

    xx

--- src/view/client/index_change.vue
+++ src/view/client/index_change.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-content">
-        <mt-header fixed title="客 户">
+        <mt-header fixed title="客户">
             <router-link to="/client" slot="left">
                 <mt-button icon="back"> </mt-button>
             </router-link>
