418d9f4514d6c26e185a3c6ec89a6f6ef5523558
WXY <wangxinyu@chinamobo.com>
2018-05-11 12:49:33 +0800

    XX

--- src/view/client/index_change.vue
+++ src/view/client/index_change.vue
@@ -196,7 +196,7 @@
                 });
 
                 // 看支持不支持FileReader  
-                // if (!file || !window.FileReader) return;
+                if (!file || !window.FileReader) return;
 
                 if (/^image/.test(file.type)) {
                     // 创建一个reader  
@@ -227,6 +227,55 @@
             },
             postImg(){
                 // console.log(this.headerImage)
+            },
+            rotateImg (img, direction,canvas) {
+                //最小与最大旋转方向，图片旋转4次后回到原方向
+                const min_step = 0;
+                const max_step = 3;
+                if (img == null)return;
+                //img的高度和宽度不能在img元素隐藏后获取，否则会出错
+                let height = img.height;
+                let width = img.width;
+                let step = 2;
+                if (step == null) {
+                step = min_step;
+                }
+                if (direction == 'right') {
+                step++;
+                //旋转到原位置，即超过最大值
+                step > max_step && (step = min_step);
+                } else {
+                step--;
+                step < min_step && (step = max_step);
+                }
+                //旋转角度以弧度值为参数
+                let degree = step * 90 * Math.PI / 180;
+                let ctx = canvas.getContext('2d');
+                switch (step) {
+                case 0:
+                    canvas.width = width;
+                    canvas.height = height;
+                    ctx.drawImage(img, 0, 0);
+                    break;
+                case 1:
+                    canvas.width = height;
+                    canvas.height = width;
+                    ctx.rotate(degree);
+                    ctx.drawImage(img, 0, -height);
+                    break;
+                case 2:
+                    canvas.width = width;
+                    canvas.height = height;
+                    ctx.rotate(degree);
+                    ctx.drawImage(img, -width, -height);
+                    break;
+                case 3:
+                    canvas.width = height;
+                    canvas.height = width;
+                    ctx.rotate(degree);
+                    ctx.drawImage(img, -width, 0);
+                    break;
+                }
             },
             compress(img, Orientation) {
                 let canvas = document.createElement("canvas");
@@ -292,7 +341,6 @@
                 console.log('压缩后：' + ndata.length);
                 console.log('压缩率：' + ~~(100 * (initSize - ndata.length) / initSize) + "%");
                 tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
-                alert(ndata)
                 return ndata;
                 
             },
