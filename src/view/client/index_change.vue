904c013cf9978575eb9c95a587c7406cb9bb1d70
WXY <wangxinyu@chinamobo.com>
2018-05-14 16:44:16 +0800

    xx

--- src/view/client/index_change.vue
+++ src/view/client/index_change.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="page-content page-main bottom0">
+    <div class="page-content">
         <mt-header fixed title="客户">
             <router-link to="/client" slot="left">
                 <mt-button icon="back"> </mt-button>
             </router-link>
             <mt-button @click="handleSave" slot="right" style="color:#333;">保存</mt-button>
         </mt-header>
-        <!-- :style="{ height: wrapperHeight + 'px' }" -->
-        <div class="wrapper"  >
+
+        <div class="page-main wrapper"  :style="{ height: wrapperHeight + 'px' }">
             <div class="main-list">
                 <div class="">
                     <div class="sc">
