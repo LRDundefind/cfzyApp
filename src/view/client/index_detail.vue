3190daf8727675966a61d297b2f5b5ee1d19d415
WXY <wangxinyu@chinamobo.com>
2018-05-15 09:29:16 +0800

    XX

--- src/view/client/index_detail.vue
+++ src/view/client/index_detail.vue
@@ -75,7 +75,7 @@
             }
         },
         mounted () {
-           this.wrapperHeight = document.documentElement.clientHeight - 280;
+           this.wrapperHeight = document.documentElement.clientHeight - 315;
             // this.$nextTick(() => {
             //    this.scroll = new BScroll(this.$refs.wrapper,{});
             // })
