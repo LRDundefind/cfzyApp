47506d54646d9037900a9ce4cc44aeca21c92118
shibeibei <shibeibei@chinamobo.com>
2018-05-24 19:37:00 +0800

    环境配置

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {
       '/rest': {
         target: 'http://47.106.86.30:8080/',
-        changeOrigin: true,
+          secure: false,      // 如果是https接口，需要配置这个参数
+          changeOrigin: true,
         pathRewrite: {
           '^/rest': '/'
         }
--- config/test.env.js
+++ config/test.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"test"',
-    BASE_PATH: '"https://119.23.251.28/headImg/"',
-    BASE_API: '"https://119.23.251.28/"',
+    BASE_PATH: '"https://xdy-tmp.52cfzy.com/headImg/"',
+    BASE_API: '"https://xdy-tmp.52cfzy.com/"',
 }
