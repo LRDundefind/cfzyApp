0dbfe6af7d2818431a779493ad7ce57e606acdad
shibeibei <shibeibei@chinamobo.com>
2018-06-13 14:41:25 +0800

    下单

--- src/view/order/orderForm.vue
+++ src/view/order/orderForm.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<div style="background: #f5f5f5;height: 100%;">
+	<div style="background: #f5f5f5;height: 100%; overflow-y: hidden">
 		
 		<mt-field label="货品名称" readonly disableClear v-model="form.goodName" class="order-p-120 order-border"></mt-field>
 		<mt-field class="form.sellUnit order-border" label="售卖单位" readonly>
@@ -16,7 +16,7 @@
 		</mt-field>
 		<mt-field class=" order-p-120 order-border" label="单价" placeholder="请输入单价" type="number" v-model="form.price"></mt-field>
 		<mt-field class="order-border" label="件数" :placeholder="surplus" type="number" v-model="form.goodNum"><span class="f-s-15 m-l-10 c-6">件</span></mt-field>
-		<mt-field label="重量" placeholder="请输入重量" type="number" v-model="form.weight">
+		<mt-field class="order-border" label="重量" placeholder="请输入重量" type="number" v-model="form.weight">
 			<select v-model="form.weight_util" class="m-l-10 jin-select">
 				<option value="unit_jin">斤</option>
 				<option value="unit_kg">公斤</option>
@@ -25,7 +25,7 @@
                  src="../../assets/index/gray-right-icon.png"/>
 
 		</mt-field>
-		<mt-field class="order-m-20 order-border" label="平板重" placeholder="请输入平板重" type="number" v-model="form.slabWeight">
+		<mt-field class=" order-border" label="平板重" placeholder="请输入平板重" type="number" v-model="form.slabWeight">
 			<span class="m-l-10 c-6 f-s-15">{{form.weight_util | sellNnit}}</span>
 		</mt-field>
 		<mt-field class="order-border" label="减水重" placeholder="" type="number" v-model="form.slushing">
@@ -285,13 +285,13 @@ export default {
     }
 
     .jin-select{
-        width: 0.85rem;
+        width: 0.95rem;
     }
 	select{
         font-size: 0.30rem;
         position: relative;
 		margin-left: 10px;
-		width: 1.3rem;
+		width: 1.4rem;
 		height: 40px;
 		line-height: 40px;
         border: none;
