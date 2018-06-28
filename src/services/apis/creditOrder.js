f8023ab0bbb06b496fce53340d18bc9adbcdc798
shibeibei <shibeibei@chinamobo.com>
2018-06-28 19:25:33 +0800

    现结收账接口

--- src/services/apis/creditOrder.js
+++ src/services/apis/creditOrder.js
@@ -34,5 +34,31 @@ export const creditOrder = {
             data: serverMoudle(params) || {}
         });
     },
+    //获取现结收账列表
+    getKnot(params) {
+        return axios({
+            url: 'rest/CHENGFENG_SET_SELEMAN_KNOT_LIST',
+            method: 'post',
+            data: serverMoudle(params) || {}
+        });
+    },
+    //获取现结收账列表
+    clearingKnot(params) {
+        return axios({
+            url: 'rest/CHENGFENG_SET_SELEMAN_KNOTCLEAR',
+            method: 'post',
+            data: serverMoudle(params) || {}
+        });
+    },
+    //取消订单
+    cancelKnot(params) {
+        return axios({
+            url: 'rest/CHENGFENG_SELEMAN_CANCEL_ORDER',
+            method: 'post',
+            data: serverMoudle(params) || {}
+        });
+    },
+
+
 
 };
