2319319f6c61985ebab7276521b7cadfd5ff4e61
liruidong <286182399@qq.com>
2018-07-17 18:31:57 +0800

    冲突

--- src/view/order/order.vue
+++ src/view/order/order.vue
@@ -1025,19 +1025,6 @@ i{
 			}
 			span:first-child{
 				padding-left: 0.25rem;
-<<<<<<< HEAD
-=======
-				word-wrap: break-word;
-				/*text-overflow:ellipsis;
-				overflow:hidden;
-				white-space:nowrap;*/ 
-				word-break: break-all;
-			    text-overflow: ellipsis;
-			    display: -webkit-box; /** 对象作为伸缩盒子模型显示 **/
-			    -webkit-box-orient: vertical; /** 设置或检索伸缩盒对象的子元素的排列方式 **/
-			    -webkit-line-clamp: 2; /** 显示的行数 **/
-			    overflow: hidden;  /** 隐藏超出的内容 **/
->>>>>>> v2.3
 			}
 		}
 		.title{
